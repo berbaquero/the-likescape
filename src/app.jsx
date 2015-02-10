444e0439eb4380bd4e9c48f19cbbcd9758f43ed1
Bernardo Baquero Stand <b.baquero.stand@tmg.nl>
2015-02-10 23:45:31 +0100

    Modularisation intensifies

--- src/app.jsx
+++ src/app.jsx
@@ -1,57 +1,10 @@
-var React = require('react');
+var React = require('react'),
+	UserToken = require('./UserToken'),
+	// Components
+	LoginButton = require('./LoginButton.jsx'),
+	Gallery = require('./Gallery.jsx');
 
-var store = window.localStorage;
-
-var CONFIG = {
-	clientID: 'a83f87bc74c94b7ca864d1cf635974f8',
-};
-
-var URL = {
-	base: 'https://api.instagram.com/v1',
-	auth: 'https://api.instagram.com/oauth/authorize?client_id=',
-	redirectParam: '&redirect_uri=',
-	responseType: '&response_type=token',
-	redirect: 'http://oogwenklikes.dev/',
-	userLikes: '/users/self/media/liked?access_token='
-};
-
-var UserToken = {
-
-	key: '',
-
-	tryObtain() {
-		// Get from Store
-		var token = this.get();
-		if (token) {
-			return true;
-		}
-
-		// Get from Instagram
-		token = location.hash;
-		if (token) {
-			var match = token.match(/access_token=(.*)/);
-			if (match) {
-				var userToken = match[1];
-				store.setItem('user:token', userToken);
-				this.key = userToken;
-				return true;
-			}
-		}
-
-		// Force Login flow
-		return false;
-	},
-
-	get() {
-		var keyValue = store.getItem('user:token');
-		if (keyValue) {
-			this.key = keyValue;
-		}
-		return this.key;
-	}
-};
-
-var Authenticator = React.createClass({
+var App = React.createClass({
 
 	getInitialState() {
 		return {
@@ -67,86 +20,11 @@ var Authenticator = React.createClass({
 	},
 
 	render() {
-
-		return (
-			this.state.authenticated ? <Gallery/> :
-				<a className="login-button" href={URL.auth + CONFIG.clientID +
-				URL.redirectParam + URL.redirect + URL.responseType}>Login</a>
-		)
-	}
-});
-
-var Image = React.createClass({
-
-	imageStyles: {
-		opacity: '0',
-		maxWidth: '100%',
-		transition: 'opacity 300ms ease 300ms'
-	},
-
-	getInitialState() {
-		return {
-			styles: this.imageStyles
+		if (this.state.authenticated) {
+			return <Gallery count='30' />
+		} else {
+			return <LoginButton/>
 		}
-	},
-
-	revealAnimation() {
-		this.state.styles.opacity = '1';
-		this.setState({
-			styles: this.state.styles
-		});
-	},
-
-	render() {
-		return (
-			<div>
-				<img className="image" src={this.props.source} onLoad={this.revealAnimation} style={this.state.styles}/>
-				<div>{this.props.title}</div>
-			</div>
-		)
-	}
-});
-
-var Gallery = React.createClass({
-
-	getInitialState() {
-		return {
-			photos: []
-		}
-	},
-
-	componentDidMount() {
-		var dis = this;
-		JSONP.get(URL.base + URL.userLikes + UserToken.key, function(response) {
-			console.log(response.data);
-			dis.setState({
-				photos: response.data
-			});
-		});
-	},
-
-	render() {
-		var images = this.state.photos.map(function(photo) {
-			return (
-				<Image source={photo.images.standard_resolution.url}
-					title={photo.caption.text} />
-			)
-		});
-
-		return (
-			<div className="gallery">
-				{images}
-			</div>
-		);
-	}
-});
-
-var App = React.createClass({
-
-	render() {
-		return (
-			<Authenticator/>
-		)
 	}
 });
 

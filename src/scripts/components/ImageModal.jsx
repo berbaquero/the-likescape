ba8489998b15b90aac9a8d271cf2430b89ea441d
Bernardo Baquero Stand <b.baquero.stand@tmg.nl>
2015-03-03 19:53:40 +0100

    Made whole modal dismiss-able by click

--- src/scripts/components/ImageModal.jsx
+++ src/scripts/components/ImageModal.jsx
@@ -2,17 +2,12 @@ var React = require('react');
 
 var Modal = React.createClass({
 
-	killClick(ev) {
-		ev.stopPropagation();
-	},
-
 	render() {
 		return (
 			<div className='modal'
 				onClick={this.props.onRequestClose}>
 				<img className='image-modal'
-					src={this.props.photoURL}
-					onClick={this.killClick} />
+					src={this.props.photoURL} />
 			</div>
 		)
 	}

bcc3736cd2e00e2cf395f3ee94f2506eaf562693
Bernardo Baquero Stand <b.baquero.stand@tmg.nl>
2015-08-10 18:19:46 +0200

    Wrapped images in <a>nchors, and made them keyboard-navigatable.

--- src/scripts/components/Image.jsx
+++ src/scripts/components/Image.jsx
@@ -15,20 +15,24 @@ const Image = React.createClass({
 		});
 	},
 
+	handleClick(ev) {
+		ev.preventDefault();
+		this.props.onClick();
+	},
+
 	render() {
 		var data = this.props.data;
-		var classes = data.type === 'video' ?
-			'image-wrap video-overlay' : 'image-wrap';
 
 		return (
-			<div className={classes}
-				 onClick={this.props.onClick}>
+			<a className={'ui-blck ' + (data.type === 'video' ? 'image-wrap video-overlay' : 'image-wrap')}
+			   href='#'
+			   onClick={this.handleClick}>
 
 				<img src={data.images.standard_resolution.url}
 					 onLoad={this.revealAnimation}
 					 className='image-item'
 					 style={this.state.styles}/>
-			</div>
+			</a>
 		)
 	}
 });
